1cfa7d8c528e51a4c1d624c63ab8e593c00306ab
Richmond Steele <not2excel@gmail.com>
2015-05-27 00:32:48 -1000

    GangCoordinator should be fleshed out enough for you carter. there's a comment about the logic being funny in it, but that can be fixed tmrw. not all the stub getters are there, but should be simple enough just to build some if you need them

--- src/main/java/com/minecave/gangs/gang/GangCoordinator.java
+++ src/main/java/com/minecave/gangs/gang/GangCoordinator.java
@@ -8,5 +8,30 @@
  */
 package com.minecave.gangs.gang;
 
+import lombok.Getter;
+import org.bukkit.Chunk;
+import org.bukkit.block.Block;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class GangCoordinator {
+
+    @Getter
+    private final Map<String, Gang> gangMap;
+
+    public GangCoordinator() {
+        gangMap = new HashMap<>();
+    }
+
+    //probably should fix the logic here, its kinda backwards lol
+
+    public boolean isChunkAvailable(Chunk chunk) {
+        return gangMap.values().stream().noneMatch(g -> g.isChunkClaimed(chunk));
+    }
+
+    public boolean isChunkAvailable(Block block) {
+        return gangMap.values().stream().noneMatch(g -> g.isChunkClaimed(block));
+    }
 }
+
