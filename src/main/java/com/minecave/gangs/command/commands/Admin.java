73cfdafa34295c01c57e97085b937bc0b67cd453
ktar5 <car1283>
2015-05-28 18:06:05 -0700

    switching to desktop

--- src/main/java/com/minecave/gangs/command/commands/Admin.java
+++ src/main/java/com/minecave/gangs/command/commands/Admin.java
@@ -6,35 +6,36 @@ import org.bukkit.entity.Player;
  * Created by Carter on 5/27/2015.
  */
 public class Admin {
-    public static void breakIn(Player player, String para) {
+
+    public static void breakIn(Player player, String faction) {
         
     }
 
-    public static void disband(Player player, String para, boolean b) {
+    public static void disband(Player player, String faction, boolean silent) {
     }
 
-    public static void power(Player player, String para) {
+    public static void power(Player player, String hoodlum) {
     }
 
-    public static void info(Player player, String para) {
+    public static void info(Player player, String faction) {
     }
 
-    public static void join(Player player, String para) {
+    public static void join(Player player, String faction) {
     }
 
-    public static void kick(Player player, String para) {
+    public static void kick(Player player, String faction) {
     }
 
-    public static void setMaxPower(Player player, String para, boolean b) {
+    public static void setMaxPower(Player player, String para, boolean hoodlum) {
     }
 
-    public static void setPower(Player player, String para, boolean b) {
+    public static void setPower(Player player, String para, boolean hoodlum) {
         
     }
 
-    public static void takePower(Player player, String para, boolean b) {
+    public static void takePower(Player player, String para, boolean hoodlum) {
     }
 
-    public static void addPower(Player player, String para, boolean b) {
+    public static void addPower(Player player, String para, boolean hoodlum) {
     }
 }
