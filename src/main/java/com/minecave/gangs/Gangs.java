56940708655b458f9b3ba42bde5553a1e30ef4c8
Richmond Steele <not2excel@gmail.com>
2015-05-28 15:19:36 -1000

    damnit forgot to add this last commit

--- src/main/java/com/minecave/gangs/Gangs.java
+++ src/main/java/com/minecave/gangs/Gangs.java
@@ -3,6 +3,7 @@ package com.minecave.gangs;
 import com.minecave.gangs.gang.GangCoordinator;
 import com.minecave.gangs.gang.HoodlumCoordinator;
 import com.minecave.gangs.storage.CustomConfig;
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +13,14 @@ public class Gangs extends JavaPlugin {
 
     private HoodlumCoordinator hoodlumCoordinator;
     private GangCoordinator gangCoordinator;
+
+    @Getter
+    private Gangs instance = null;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
 
     @Override
     public void onEnable() {
