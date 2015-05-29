b829003b4ee2a7aff40eeea8411a6d681a24247d
Ktar5 <buildfresh@gmail.com>
2015-05-28 18:30:15 -0700

    Removed stuff

--- src/main/java/com/minecave/gangs/Gangs.java
+++ src/main/java/com/minecave/gangs/Gangs.java
@@ -4,7 +4,6 @@ import com.minecave.gangs.gang.GangCoordinator;
 import com.minecave.gangs.gang.HoodlumCoordinator;
 import com.minecave.gangs.storage.CustomConfig;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Gangs extends JavaPlugin {
@@ -33,19 +32,13 @@ public class Gangs extends JavaPlugin {
         hoodlumCoordinator = new HoodlumCoordinator(this);
         gangCoordinator = new GangCoordinator(this);
 
-        chests = new CustomConfig(getDataFolder(), "chests.yml");
+        config = new CustomConfig(getDataFolder(), "config.yml");
         messages = new CustomConfig(getDataFolder(), "messages.yml");
 
-        Loader.load();
-
-        Bukkit.getServer().getPluginManager().registerEvents(new LogOffListener(), this);
-        Bukkit.getServer().getPluginManager().registerEvents(new InteractListener(), this);
-        getCommand("rc").setExecutor(new CommandListener());
     }
 
     @Override
     public void onDisable() {
-        Loader.unload();
         instance = null;
     }
 
