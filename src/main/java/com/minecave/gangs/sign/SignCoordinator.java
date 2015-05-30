a574743ee9e38493f0db85d63105580c10141dee
Ktar5 <buildfresh@gmail.com>
2015-05-30 03:31:09 -0700

    Signs

--- src/main/java/com/minecave/gangs/sign/SignCoordinator.java
+++ src/main/java/com/minecave/gangs/sign/SignCoordinator.java
@@ -9,14 +9,60 @@
 package com.minecave.gangs.sign;
 
 import com.minecave.gangs.Gangs;
+import com.minecave.gangs.gang.Hoodlum;
+import com.minecave.gangs.util.StringUtil;
 import lombok.Getter;
+import org.bukkit.block.Sign;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class SignCoordinator {
 
     @Getter
     private final Gangs plugin;
 
+    @Getter
+    private List<Sign> signs;
+
+
     public SignCoordinator(Gangs plugin) {
         this.plugin = plugin;
+        this.signs = new ArrayList<>();
+    }
+
+    public void load(){
+        for(String[] signLines : getSignsAndTheirLinesAndShitRichFillThisOut()){
+            signs.add(StringUtil.colorArray(signLines));
+        }
+    }
+
+    /**
+     * remove a sign from hoodlum gang #56431
+     *
+     * @param hoodlum The sign to hoodlumize
+     * @return If the hoodlumizationicar was successimical
+     */
+    public boolean removeSign(Sign hoodlum) {
+        if (signs.contains(hoodlum)) {
+            signs.remove(hoodlum);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Add a sign to hoodlum gang #56431
+     *
+     * @param hoodlum The sign to hoodlumize
+     * @return If the hoodlumizationicar was successimical
+     */
+    public boolean addSign(Sign hoodlum) {
+        if (!signs.contains(hoodlum)) {
+            signs.add(hoodlum);
+            return true;
+        }
+        return false;
     }
 }
