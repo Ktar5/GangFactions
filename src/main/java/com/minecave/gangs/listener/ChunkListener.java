65a2fd15be22deac1bc0b303f88df65829d16765
ktar5 <carcar1283>
2015-08-29 13:19:36 -0700

    Fixed adding new crops

--- src/main/java/com/minecave/gangs/listener/ChunkListener.java
+++ src/main/java/com/minecave/gangs/listener/ChunkListener.java
@@ -8,6 +8,7 @@
  */
 package com.minecave.gangs.listener;
 
+import com.google.common.collect.Lists;
 import com.minecave.gangs.Gangs;
 import com.minecave.gangs.gang.Gang;
 import com.minecave.gangs.gang.Hoodlum;
@@ -23,9 +24,17 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.List;
+
 public class ChunkListener implements Listener {
 
     private final Gangs plugin;
+
+    List<Material> material = Lists.newArrayList(Material.SUGAR_CANE, Material.SUGAR_CANE_BLOCK,
+            Material.BROWN_MUSHROOM, Material.CACTUS, Material.CARROT, Material.CARROT_ITEM, Material.COCOA,
+            Material.CROPS, Material.MELON_SEEDS, Material.NETHER_WARTS, Material.POTATO, Material.POTATO_ITEM, Material.YELLOW_FLOWER,
+            Material.WHEAT, Material.PUMPKIN_SEEDS, Material.PUMPKIN_STEM, Material.MELON_STEM, Material.RED_MUSHROOM,
+            Material.RED_ROSE);
 
     public ChunkListener(Gangs plugin) {
         this.plugin = plugin;
@@ -68,13 +77,13 @@ public class ChunkListener implements Listener {
         }
 
 
-        if (event.getBlock().getType() != Material.SUGAR_CANE_BLOCK ||
-                event.getBlock().getType() != Material.SUGAR_CANE) {
+        if (!material.contains(event.getBlock().getType())) {
             return;
         }
         Material blockBelow = event.getBlock().getLocation().subtract(0, 1, 0).getBlock().getType();
         if (blockBelow == Material.SUGAR_CANE_BLOCK ||
-                blockBelow == Material.SUGAR_CANE) {
+                blockBelow == Material.SUGAR_CANE ||
+                blockBelow == Material.CACTUS) {
             return;
         }
         Chunk chunk = event.getBlock().getChunk();
@@ -92,8 +101,7 @@ public class ChunkListener implements Listener {
         if(isWorldAllowed(event.getBlockPlaced().getWorld())){
             return;
         }
-        if (event.getBlockPlaced().getType() != Material.SUGAR_CANE_BLOCK &&
-                event.getBlockPlaced().getType() != Material.SUGAR_CANE) {
+        if (!material.contains(event.getBlockPlaced().getType())) {
             return;
         }
         Chunk chunk = event.getBlock().getChunk();
